57128477b7fe68d89f766a044502db68b8e865d8
Timot656 <timosheats@gmail.com>
2025-02-10 15:40:27 -0500

    Add files via upload

---
+++ typecastin.py
@@ -0,0 +1,20 @@
+name= "Joe"
+age = 7
+is_student = True
+weight = 21.6
+print("Name:" , name)
+print("Data Type of Name is", type(name))
+print("Age : ", age)
+print("Data Type of Age is" ,type(age))
+print("is_student : ", is_student)
+print("Data Type of is_student is", type(is_student))
+print("weight", weight)
+print("Data Type of weight is" , type(weight))
+#Type Casting to cnvert the data type of variables
+print("\n After Type Casting.....")
+age = str(age)
+print(age)
+print("Data Type of age is: ", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight is: ",type(weight))
