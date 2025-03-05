ec3186eb69743fbab12a9e13a7c8b56d58764ede
Timot656 <timosheats@gmail.com>
2025-03-05 09:31:41 -0500

    Add files via upload

---
+++ AND OR .py
@@ -0,0 +1,19 @@
+a = 10
+b = 12
+c =0
+if a and b and c:
+    print("ALL THE NUMBERS HAVE BOOLEAN VALUE AS TRUE")
+else:
+    print("ALEAST ONE NUMBER HAS BOOLEAN VALUE AS FALSE")
+a = 10 
+b = 12
+c = 0
+if a or b or c:
+    print("either of the number is greater then 0")
+else:
+    print("No number is greater than 0")
+if b > 0 or c > 0:
+    print("Either of the number if greater than 0")
+else: 
+    print("No number is greater than 0")
+ 
