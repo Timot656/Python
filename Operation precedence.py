6f935648a31881eb03167740776eb62a639c1c17
Timot656 <timosheats@gmail.com>
2025-03-12 09:13:00 -0400

    Add files via upload

---
+++ Operation precedence.py
@@ -0,0 +1,13 @@
+v = 4
+w = 5
+x = 8 
+y = 2
+z = 0
+z =(v +w) *x/y;
+print("Value of (v + w) * x / y is ", z)
+name = "John"
+age = 0
+if name == "John" or name == "Rick" and age >= 2:
+    print("Hello John!")
+else:
+    print("Good Bye!")
