c777b84f5f77d6c80b7e0d8d1b7d36984e06ad1d
Timot656 <timosheats@gmail.com>
2025-03-16 21:44:51 -0400

    Add files via upload

---
+++ Acii value.py
@@ -0,0 +1,4 @@
+character = (input("Enter your character: "))
+ascii_value = ord(character)
+print(f"The ASCII value of '{character}' is {ascii_value}")
+# Expected output: The ASCII value of 'A' is 65
