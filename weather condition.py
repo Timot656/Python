e673b39ed2ef681d553aeb9df1ed6e1b9bf47485
Timot656 <timosheats@gmail.com>
2025-04-11 09:23:28 -0400

    Add files via upload

--- weather condition.py
+++ weather condition.py
@@ -1,6 +1,7 @@
 def weather_condition():
     print("The weather is pleasant in: ", spring)
     print("The weather is same in: ", autum)
-    spring = autum
-    autum = spring
-    weather_condition()
+spring = "autum"
+autum = spring
+weather_condition()
+    
