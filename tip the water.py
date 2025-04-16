9b6df35ea4bcf58c3f3bb8828a836cb49b269b26
Timot656 <timosheats@gmail.com>
2025-04-16 09:18:09 -0400

    Add files via upload

--- tip the water.py
+++ tip the water.py
@@ -1,5 +1,5 @@
 def total_calc(Bill_amount,tip_perc):
     total = Bill_amount *(1 + 0.01 * tip_perc)
     total = round(total,2)
-    print(f"Please pay $(total)")
+    print(f"Please pay ${total}")
 total_calc(150,20)
