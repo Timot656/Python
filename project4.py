bb3bb30f2815cd44aa83e572a3443d5faf535e46
Timot656 <timosheats@gmail.com>
2025-04-16 09:13:21 -0400

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,6 @@
+import math
+def calculate_circumference(radius):
+    return 2 * math.pi * radius
+radius = 5  
+circumference = calculate_circumference(radius)
+print(f"The circumference of the circle with radius {radius} is {circumference:.2f}")
