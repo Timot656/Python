5ce1414f0a06e7192df10095d1e54579cf4eab7d
Timot656 <timosheats@gmail.com>
2025-05-19 10:08:40 -0400

    Add files via upload

---
+++ set iner.py
@@ -0,0 +1,8 @@
+setx = {"Green, blue"}
+sety = {"Blue, yellow"}
+print("Oringinal set elemets")
+print(setx)
+print(sety)
+print("\n Intersection of two said sets: ")
+setz = setx.intersection(sety)
+print(setz)
