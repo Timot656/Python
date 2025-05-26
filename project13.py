e080829176df21b8c1aa8d5b7b437862ce705364
Timot656 <timosheats@gmail.com>
2025-05-26 09:32:45 -0400

    Add files via upload

---
+++ project13.py
@@ -0,0 +1,6 @@
+def symmetric_difference(set1, set2):
+    return set1.symmetric_difference(set2)
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+result = symmetric_difference(set1, set2)
+print("Symmetric Difference:", result)
