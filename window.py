a0c4ec0e259542f33f65a8d2ba8cab394fa51c97
Timot656 <timosheats@gmail.com>
2025-07-23 10:45:43 -0400

    Add files via upload

--- window.py
+++ window.py
@@ -1,13 +1,41 @@
 import pygame
 pygame.init()
 
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
 screen = pygame.display.set_mode((400,500))
 
-done = False
+pygame.display.set_caption('Adding image and background image')
 
-while not done:
+Background_image = pygame.transform.scale(pygame.image.load('Backround.png').convert(),(SCREEN_WIDTH, SCREEN_HEIGHT))
+
+penguin_image = pygame.transform.scale(pygame.image.load('penguin.png').convert_alpha(), (200, 200))
+penguin_rect = penguin_image.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 30))
+
+text = pygame.font.Font(None, 36).render('Hello World' , True, pygame.Color('black'))
+text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 110))        
+
+def game_loop():
+    clock = pygame.time.Clock()
+    running = True
+    while running:display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
- 
+            running = False
+            
+    display_surface.blit(Background_image, (0, 0))
+    display_surface.blit(penguin_image, penguin_rect)
+    display_surface.blit(text, text_rect)
+    
     pygame.display.flip()
+    clock.tick(30)
+    
+pygame.quit()
+
+if __name__ == '__main__':
+    game_loop()
